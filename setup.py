04a768baf091c7f6a883d329c9d6cedd15fa3efa
Daniel Smedema <djsmedes@gmail.com>
2014-08-13 13:24:11 +0200

    added setup.py
    ...

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='thepyetribe',
+      version='1.0',
+      description='Python wrapper for The Eye Tribe eye tracker',
+      author='Daniel Smedema',
+      author_email='djsmedes@gmail.com',
+      url='https://github.com/djsmedes/thepyetribe',
+      license='GNU GPL Version 3',
+      py_modules=['thepyetribe'],
+      )
